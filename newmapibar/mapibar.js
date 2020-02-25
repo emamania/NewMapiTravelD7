457d12aa4877861d1e760ad43425698a3b9a33cf
Elvis <centinela1526@gmail.com>
2020-02-25 11:17:52 -0500

    topbar and html

--- newmapibar/mapibar.js
+++ newmapibar/mapibar.js
@@ -3,8 +3,6 @@
 	mapiChats = function() {
 		if(Drupal.settings.mapibar) {
 			var cbwChat = Drupal.settings.mapibar.chat;
-			var cbwSkype = Drupal.settings.mapibar.skype;
-			var cbwCall = Drupal.settings.mapibar.call;
 			var cbWidth = Drupal.settings.mapibar.width;
 			// Contact bar position hack
 			var ww = $(window).width();
@@ -12,9 +10,7 @@
 			$('#contact-bar').css('width', cbWidth);
 			$('#contact-bar').css('left', cb);
 			
-			$('#cb-chat').popup(paramsChat, cbwChat);
-			$('#cb-skype').popup('location=0,status=0,scrollbars=0,width=750,height=400', cbwCall);
-			$('#cb-call').popup(paramsSkype, cbwSkype);
+			$('#cb-chat').popup(paramsChat, cbwChat);			
 			$('#cb-whatsapp').click(function(){console.log("Comunicandose por wpp");})
 						
 			
